da16a506d91f67f10bafe90485083f424bb8b03e
ShawnLiu187 <31958595+ShawnLiu187@users.noreply.github.com>
2018-11-03 16:38:16 -0400

    Updates

---
+++ precache-manifest.dff90040ea34629d527cad55f7051692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "022fa9bfdb39c5c3a072",
+    "url": "/react_routing/static/js/runtime~main.022fa9bf.js"
+  },
+  {
+    "revision": "010baedd84aa03d6cfc4",
+    "url": "/react_routing/static/js/main.010baedd.chunk.js"
+  },
+  {
+    "revision": "d47d3de026c5e734a016",
+    "url": "/react_routing/static/js/1.d47d3de0.chunk.js"
+  },
+  {
+    "revision": "010baedd84aa03d6cfc4",
+    "url": "/react_routing/static/css/main.0a4e83a0.chunk.css"
+  },
+  {
+    "revision": "12128f724bbdd6afdde2e40c9b2fce1b",
+    "url": "/react_routing/index.html"
+  }
+];
